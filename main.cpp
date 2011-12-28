bb31bf384cdebd9eede318df1a73475adb93dfe9
Neekobus <Neeko@MacBook-de-Neeko.local>
2011-12-28 23:57:47 +0100

    2nd iteration : Hero move to right or left while player press the right or left arrow

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 {
    
 	// Create main window
-    sf::RenderWindow App(sf::VideoMode(800, 600), "The Quest of the Incrementalist 1");
+    sf::RenderWindow App(sf::VideoMode(800, 600), "The Quest of the Incrementalist 2");
 	sf::Color backgroundColor = sf::Color(50, 50, 50);
 	
 	sf::Image Image;
@@ -31,6 +31,8 @@ int main()
 	Sprite.SetX((App.GetWidth() - Image.GetWidth()) / 2);
 	Sprite.SetY((App.GetHeight() - Image.GetHeight()) / 2);
 	
+	float move = 200;
+	
     // Start game loop
     while (App.IsOpened())
     {
@@ -42,6 +44,16 @@ int main()
             if (Event.Type == sf::Event::Closed)
                 App.Close();
         }
+		
+		//sf::Sleep(0.4f);
+		
+		// Get elapsed time for this frame
+		float ElapsedTime = App.GetFrameTime();
+		//float Framerate = 1.f / ElapsedTime;
+		
+		// Move the sprite
+		if (App.GetInput().IsKeyDown(sf::Key::Left))  Sprite.Move(-1 * move * ElapsedTime, 0);
+		if (App.GetInput().IsKeyDown(sf::Key::Right)) Sprite.Move( move * ElapsedTime, 0);
 
         // Clear screen
         App.Clear(backgroundColor);
