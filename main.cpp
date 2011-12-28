b075b926e989fec524b5e54fd3a2db3bc5a10bc6
Neekobus <Neeko@MacBook-de-Neeko.local>
2011-12-28 23:20:45 +0100

    1 iteration : Hero is centered on the screen.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,24 @@
 ////////////////////////////////////////////////////////////
 int main()
 {
-    // Create main window
-    sf::RenderWindow App(sf::VideoMode(640, 480), "SFML Graphics");
+   
+	// Create main window
+    sf::RenderWindow App(sf::VideoMode(800, 600), "The Quest of the Incrementalist 1");
+	sf::Color backgroundColor = sf::Color(50, 50, 50);
+	
+	sf::Image Image;
+	Image.SetSmooth(false); //to avoid any antialias
 
+	if (!Image.LoadFromFile("hero.png"))
+	{
+		return EXIT_FAILURE;
+	}
+	
+	sf::Sprite Sprite;
+	Sprite.SetImage(Image);
+	Sprite.SetX((App.GetWidth() - Image.GetWidth()) / 2);
+	Sprite.SetY((App.GetHeight() - Image.GetHeight()) / 2);
+	
     // Start game loop
     while (App.IsOpened())
     {
@@ -29,10 +44,9 @@ int main()
         }
 
         // Clear screen
-        App.Clear();
+        App.Clear(backgroundColor);
 
-        // Draw apredefined shape
-        App.Draw(sf::Shape::Circle(200, 200, 100, sf::Color::Yellow, 10, sf::Color::Blue));
+		App.Draw(Sprite);
 
         // Finally, display the rendered frame on screen
         App.Display();
